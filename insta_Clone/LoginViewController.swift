9651ff59328614b4b60488c375aa49f164903294
shankar08 <shan08kar@gmail.com>
2018-03-02 07:58:33 -0500

    sign In added

---
+++ insta_Clone/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  insta_Clone
+//
+//  Created by Champ on 3/2/18.
+//  Copyright © 2018 Shankar Prajapati. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+    @IBAction func onLoginBtnPressed(_ sender: Any) {
+        
+    }
+    
+    
+    
+    func Login(){
+        
+    }
+
+
+}
